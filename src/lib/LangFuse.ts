fa300dcaf99de3a8454fd839e7905c8b1d4eeeb0
sgssrg <sourikghosh08@gmail.com>
2026-06-02 23:50:09 +0530

    enhance: LangFuse Added

---
+++ src/lib/LangFuse.ts
@@ -0,0 +1,4 @@
+import { CallbackHandler } from "@langfuse/langchain";
+
+// Initialize the Langfuse CallbackHandler
+export const LangFuse = new CallbackHandler();
---
+++ src/lib/instrumentation.ts
@@ -0,0 +1,9 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { LangfuseSpanProcessor } from "@langfuse/otel";
+
+const sdk = new NodeSDK({
+  spanProcessors: [new LangfuseSpanProcessor()],
+});
+
+sdk.start();
+
